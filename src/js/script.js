c3b807b9bbf81e016d21574fefd4c6874f84dd7f
Rafael <rafinhaacarmona@gmail.com>
2025-05-14 18:54:47 -0300

    Criando a função slide show

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
+//DECLARANDO O ARRAY DE IMAGENS
 
+let imagens =[
+    'src/assets/carro1.jpg',
+    'src/assets/carro2.jpg',
+    'src/assets/carro3.jpg',
+];
+
+//DECLARANDO AS VARIAVEIS
+
+let index=0;
+let tempo=3000;
+
+//CRIANDO A FUNÇÃO SLIDESHOW
+
+function slideShow(){
+    document.getElementById("image").src=imagens[index];
+    index++;
+
+    if(index == imagens.length){
+        index=0;
+    }
+    setTimeout("slideShow()",tempo)
+}
+slideShow();
